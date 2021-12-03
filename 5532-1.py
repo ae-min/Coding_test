131943597d524971d34e93975d5a9e4736d61aa6
ae-min <papace@naver.com>
2021-12-03 22:34:43 +0900

    21-12-01

---
+++ 5532-1.py
@@ -0,0 +1,15 @@
+import math
+L = int(input())
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+k = math.ceil(A / C)
+m = math.ceil(B / D)
+free = max(k, m)
+print(L - free)
+
+'''
+math 모듈의 ceil함수를 이용해 딱떨어지지 않을경우 올림 가능
+'''
+
