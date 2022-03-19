47390d61da4c5ed95de5a991343f79461ea143d8
ae-min <papace@naver.com>
2022-03-19 21:51:43 +0900

    22-03-19

--- 10474.py
+++ 10474.py
@@ -1,5 +1,11 @@
-while 1:
+while True:
     a, b = map(int, input().split())
-    if a == b == 0:
+    
+    if a==0 and b==0:
         break
-    print(f"{a//b} {a%b} / {b}")
+        
+    x = a//b
+    y = a%b
+
+    print(x, y,end=" / ")
+    print(b)
